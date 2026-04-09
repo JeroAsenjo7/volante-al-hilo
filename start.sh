43a16eca73f0934191548f3e7b8b3e856a1f0db3
JeroAsenjo7 <jeroasenjo@gmail.com>
2026-04-09 20:54:02 -0300

    crear superusuario via shell

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-python create_superuser.py
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.filter(username='admin').first(); u.set_password('contraseña') if u else User.objects.create_superuser('admin', 'admin@email.com', 'contraseña'); u.save() if u else None; print('Listo')"
 gunicorn volante_al_hilo.wsgi --log-file -
