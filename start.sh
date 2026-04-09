735b78c1eb19ff1723bc689bbfbf96963f58bab8
JeroAsenjo7 <jeroasenjo@gmail.com>
2026-04-09 20:57:44 -0300

    fix encoding contrasena

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+﻿#!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.filter(username='admin').first(); u.set_password('contraseña') if u else User.objects.create_superuser('admin', 'admin@email.com', 'contraseña'); u.save() if u else None; print('Listo')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.filter(username='admin').first(); u.set_password('Admin1234') if u else User.objects.create_superuser('admin', 'admin@email.com', 'Admin1234'); u.save() if u else None; print('Listo')"
 gunicorn volante_al_hilo.wsgi --log-file -
