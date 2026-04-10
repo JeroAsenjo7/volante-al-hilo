7ca21c734cb31bdc2fe319fbdabad106f2883482
JeroAsenjo7 <jeroasenjo@gmail.com>
2026-04-09 21:07:34 -0300

    agregar fixture stock inicial

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 ﻿#!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.filter(username='admin').first(); u.set_password('Admin1234') if u else User.objects.create_superuser('admin', 'admin@email.com', 'Admin1234'); u.save() if u else None; print('Listo')"
+python manage.py loaddata stock/fixtures/stock_inicial.json
 gunicorn volante_al_hilo.wsgi --log-file -
